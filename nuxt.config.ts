cab99557b84e7330e1ff17683421e1c6925a261a
wuwei <wuwei@fadada.com>
2025-08-01 11:27:17 +0800

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false,
+    },
+  },
+});
